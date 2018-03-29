3c738f537dadc3e861c381afc128894c8996c793
朝歌 <amtlib-dot-dll@users.noreply.github.com>
2018-03-29 08:02:17 +0800

    Update flatpak.sh

--- flatpak.sh
+++ flatpak.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
+(while true; do sleep 300; done) &
 flatpak remote-add --user --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-flatpak-builder --arch=x86_64 --user --install-deps-from=flathub --sandbox --repo=repo build *.json
+flatpak-builder --arch=x86_64 --user --install-deps-from=flathub --sandbox --repo=repo build *.json >/dev/null 2>/dev/null
+flatpak repo --info repo
